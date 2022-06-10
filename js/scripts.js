28cbeb6ca006566c874d6a84a0ce4612c6c837b7
Doston04 <avazovjr@gmail.com>
2022-06-10 15:34:37 +0500

    mobile-header-added

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector(".hamburger")
 const span1 = document.querySelector("#span1")
 const span2 = document.querySelector("#span2")
 const span3 = document.querySelector("#span3")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 hamburger.addEventListener("click", () => {
     span1.classList.toggle("rotateRight")
     span2.classList.toggle("hidden")
     span3.classList.toggle("rotateLeft")
+    mobileMenu.classList.toggle("hidden")
 })
 
 const currentYearSpan = document.querySelector("#currentYear")
